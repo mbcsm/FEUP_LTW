2566c7da8e293b330d8cce2d4b7c6563022dc6a0
DoR18696 <36338524+DoR18696@users.noreply.github.com>
2018-11-15 17:46:22 +0000

    task 1

--- database/redditx.sql
+++ database/redditx.sql
@@ -2,3 +2,17 @@ CREATE TABLE user (
   username VARCHAR PRIMARY KEY,
   password VARCHAR NOT NULL
 );
+
+CREATE TABLE story (
+  story_id INTEGER PRIMARY KEY,
+  story_tittle VARCHAR NOT NULL,
+  story_text VARCHAR NOT NULL,
+  username VARCHAR NOT NULL REFERENCES user
+);
+
+CREATE TABLE comment (
+  comment_id INTEGER PRIMARY KEY,
+   username VARCHAR NOT NULL REFERENCES user,
+  story_id VARCHAR NOT NULL REFERENCES story,
+  comment_text VARCHAR NOT NULL
+);
