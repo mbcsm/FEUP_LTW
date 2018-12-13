858829dbcf99f658f8265d7872747bde849a0240
mbcsm <manelbcsm1@gmail.com>
2018-12-13 09:49:00 +0000

    Timestamps added to the story and comment tables

--- database/redditx.sql
+++ database/redditx.sql
@@ -8,14 +8,16 @@ CREATE TABLE story (
   story_id INTEGER PRIMARY KEY,
   story_title VARCHAR NOT NULL,
   story_text VARCHAR NOT NULL,
-  user_id INTEGER REFERENCES user
+  user_id INTEGER REFERENCES user,
+  timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE comment (
   comment_id INTEGER PRIMARY KEY,
   comment_text VARCHAR NOT NULL,
   user_id INTEGER REFERENCES user,
-  story_id VARCHAR NOT NULL REFERENCES story
+  story_id VARCHAR NOT NULL REFERENCES story,
+  timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 INSERT INTO user (
@@ -37,3 +39,4 @@ INSERT INTO comment (
   1,
   1
 );
+
